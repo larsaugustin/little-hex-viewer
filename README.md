7139f2c2798ec3efbf229608c3e30a9d3022f593
Lars Augustin <larsaugustin@icloud.com>
2019-10-02 21:40:01 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Little Hex Viewer
+
+This is an incredibly simple hex viewer, that runs in your browser. You can see a simple demo of this running [over at Glitch](https://hex-viewer.glitch.me).
+
+## Setup
+
+Just install and run!
+
+```
+npm install
+npm start
+```
+
+You can now open a new instance of the hex editor by opening the URL printed after you started node.
+
+## Usage
+
+On the homepage, you can choose a file to look at. ***Warning:** The viewer doesn't split large files, meaning you shouldn't try to view incredibly large ones.*
+
+## Bugs?
+
+Open an issue!
+
+**I hope you find this tool helpful!**
---
+++ server.js
@@ -0,0 +1,58 @@
+// server.js
+
+// requirements
+var express = require("express");
+var stylus = require("stylus");
+var fileUpload = require("express-fileupload");
+var nib = require("nib");
+var app = express();
+
+// compiling the stylesheets
+function compile(str, path) {
+  return stylus(str)
+    .set("filename", path)
+    .use(nib());
+}
+
+// ejs + stylus setup
+app.set("view engine", "ejs");
+app.use(
+  stylus.middleware({
+    src: __dirname + "/public",
+    compile: compile
+  })
+);
+app.use(
+  fileUpload()
+);
+app.use(express.static(__dirname + "/public"));
+
+// rendering /
+app.get("/", function(request, response) {
+  response.render("pages/index");
+});
+
+app.post("/", function(request, response) {
+  if (request.files != null) {
+    const data = request.files.inputfile.data;
+    const bufferToString = Array.prototype.map.call(new Uint8Array(data), x =>
+      ("00" + x.toString(16)).slice(-2)
+    );
+    const bufferToCharacter = Array.prototype.map.call(
+      new Uint8Array(data),
+      x => String.fromCharCode(x).slice(-1)
+    );
+    response.render("pages/viewer", {
+      hexValues: bufferToString,
+      charValues: bufferToCharacter
+    });
+    console.log(bufferToCharacter);
+  } else {
+    response.render("pages/index");
+  }
+});
+
+// listener
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
